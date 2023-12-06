aec924ec2f0b73ecf6da17cf71a4bc8c2158258a
Qwerty000101 <andreyiv20005@gmail.com>
2023-12-06 11:52:28 +0300

    Feature-1_0.01

--- PyCharm_Project/hard_individual.py
+++ PyCharm_Project/hard_individual.py
@@ -1,23 +1,38 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
+from decimal import *
 import math
 import sys
 
+
+getcontext().prec = 4096
 EULER = 0.5772156649015328606
 EPS = 1e-10
 
 if __name__ == '__main__':
-    x = float(input("Value of x? "))
+    x = Decimal(input("Value of x? "))
+
     if x == 0:
         print("Illegal value of x", file=sys.stderr)
         exit(1)
 
-    a = 0
-    S, n = 0, 1
-    while math.fabs(a) > EPS:
-        a = ((-1)**n)*(x**(2*n))/((2*n)*math.factorial(2*n))
-        S += a
-        n += 1
+    S = Decimal(0)
+    a = Decimal(1)
+    n = Decimal(1)
 
-    print(f"Ci({x}) = {EULER + math.log(math.fabs(x)) + S}")
+    while Decimal(math.fabs(a)) > EPS:
+        e0 = Decimal((-1)**n)
+        e1 = Decimal(x**(2*n))
+        e2 = Decimal(2*n)
+        e3 = Decimal(1)
+        temp = 2*n
+        while temp > 1:
+             e3 *= temp
+             temp -= 1
+        a = (e0*e1)/(e2*e3)
+        n += 1
+        S += a
+
+    print(f"\nCi({x}) = {EULER + math.log(x) + float(S)}")
